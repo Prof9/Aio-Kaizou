e32a12ed01951d996c5beff8ea5b7b147d546074
Prof. 9 <prof9@outlook.com>
2017-12-08 03:59:58 +0100

    Initial commit

---
+++ patcher/Aio-Kaizou/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace AioKaizou {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button1 = new System.Windows.Forms.Button();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.groupBox1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			resources.ApplyResources(this.button1, "button1");
+			this.button1.Name = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// saveFileDialog1
+			// 
+			resources.ApplyResources(this.saveFileDialog1, "saveFileDialog1");
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButton3);
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			resources.ApplyResources(this.groupBox1, "groupBox1");
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.TabStop = false;
+			// 
+			// radioButton3
+			// 
+			resources.ApplyResources(this.radioButton3, "radioButton3");
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.TabStop = true;
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			resources.ApplyResources(this.radioButton2, "radioButton2");
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.TabStop = true;
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton1
+			// 
+			resources.ApplyResources(this.radioButton1, "radioButton1");
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.TabStop = true;
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.Name = "label1";
+			// 
+			// label2
+			// 
+			resources.ApplyResources(this.label2, "label2");
+			this.label2.Name = "label2";
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.groupBox1);
+			this.panel1.Controls.Add(this.button1);
+			resources.ApplyResources(this.panel1, "panel1");
+			this.panel1.Name = "panel1";
+			// 
+			// Form1
+			// 
+			resources.ApplyResources(this, "$this");
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ patcher/Aio-Kaizou/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AioKaizou {
+	public partial class Form1 : Form {
+		private static readonly byte[] MSG_DATA = {
+			0x02,0x00,0xF1,0xF0,0x00,0x00,0xF9,0x03,0xC3,0x01,0xF8,0x01,0x00,0xE0,0xF8,0x01,
+			0x01,0x1C,0xF8,0x01,0x02,0x00,0xF8,0x01,0x03,0x47,0xFF,0x35,0x10,0x6A,0x54,0x30,
+			0x01,0xB5,0x52,0x46,0x12,0x6F,0x10,0x78,0xFF,0x28,0x01,0xD1,0x07,0x32,0x10,0x78,
+			0xA9,0x8C,0xC9,0x08,0x04,0xD3,0x01,0x30,0x85,0x28,0x00,0xDD,0x85,0x38,0x10,0x70,
+			0x85,0x28,0x00,0xD1,0x00,0x20,0xE9,0x6C,0xE8,0x64,0x88,0x42,0x01,0xD0,0x18,0x3C,
+			0x6C,0x63,0xFE,0xB4,0x00,0x49,0x08,0x47,0x13,0x65,0x00,0x08,
+		};
+
+		private enum GameVersion {
+			EXE4v10,
+			EXE4v11,
+			MMBN4
+		}
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			GameVersion ver = 0;
+			if (radioButton1.Checked) {
+				ver = GameVersion.EXE4v10;
+			} else if (radioButton2.Checked) {
+				ver = GameVersion.EXE4v11;
+			} else if (radioButton3.Checked) {
+				ver = GameVersion.MMBN4;
+			} else {
+				if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ja") {
+					MessageBox.Show("ROMバージョンを選んでください！");
+				} else {
+					MessageBox.Show("Please select a ROM version!");
+				}
+				return;
+			}
+
+			if (saveFileDialog1.ShowDialog() != DialogResult.OK) {
+				return;
+			}
+
+			try {
+				using (FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.Open, FileAccess.ReadWrite, FileShare.None)) {
+					byte[] save = new byte[0x8000];
+					if (fs.Length < save.Length) {
+						if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ja") {
+							throw new Exception("無効なエグゼ４セーブファイル　（ファイルが短すぎます）");
+						} else {
+							throw new Exception("Not a valid MMBN4/EXE4 save file (file too short)");
+						}
+					}
+					fs.Position = 0;
+					fs.Read(save, 0, save.Length);
+
+					EnDecrypt(save);
+
+					int shuffle = BitConverter.ToInt32(save, 0x1550);
+					if (shuffle < 0 || shuffle > 0x1FC || (shuffle & 3) != 0) {
+						if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ja") {
+							throw new Exception("無効なエグゼ４セーブファイル　（無効なシャッフルオフセット）");
+						} else {
+							throw new Exception("Not a valid MMBN4/EXE4 save file (invalid shuffle offset)");
+						}
+					}
+
+					bool bluemoon;
+					uint checksum = BitConverter.ToUInt32(save, 0x21E8 + shuffle);
+					uint checksumRS = CalcChecksum(save, shuffle, ver == GameVersion.MMBN4, false);
+					uint checksumBM = CalcChecksum(save, shuffle, ver == GameVersion.MMBN4, true);
+					if (checksum == checksumRS) {
+						bluemoon = false;
+					} else if (checksum == checksumBM) {
+						bluemoon = true;
+					} else {
+						if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ja") {
+							throw new Exception("無効なエグゼ４セーブファイル　（チェックサムが一致しません）");
+						} else {
+							throw new Exception("Not a valid MMBN4/EXE4 save file (checksum does not match)");
+						}
+					}
+
+					if (!CheckSaveString(save, 0x2208 + shuffle, "ROCKMANEXE4 20031022")) {
+						if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ja") {
+							throw new Exception("無効なエグゼ４セーブファイル　（ゲームタイトルがありません）");
+						} else {
+							throw new Exception("Not a valid MMBN4/EXE4 save file (missing game title)");
+						}
+					}
+
+					WriteMsgData(save, ver);
+
+					// Set card numbers.
+					for (int i = 0; i < 6; i++) {
+						save[0x4644 + shuffle + i] = 0x00;
+						save[0x464C + shuffle + i] = 0xFF;
+						save[0x4653 + shuffle + i] = 0x85;
+					}
+
+					// Unlock all cards.
+					for (int i = 1; i <= 133; i++) {
+						save[0x5D14 + shuffle + i] = (byte)(save[0x03D0 + i] ^ (bluemoon ? 0x31 : 0x43));
+					}
+
+					// Enable kaizou menu.
+					SetFlag(save, shuffle, 0x0072, true);
+
+					checksum = CalcChecksum(save, shuffle, ver == GameVersion.MMBN4, bluemoon);
+					Array.Copy(BitConverter.GetBytes(checksum), 0, save, 0x21E8 + shuffle, sizeof(uint));
+
+					EnDecrypt(save);
+
+					fs.Position = 0;
+					fs.Write(save, 0, save.Length);
+				}
+				if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ja") {
+					MessageBox.Show("成功！");
+				} else {
+					MessageBox.Show("Success!");
+				}
+			} catch (Exception ex) {
+				if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ja") {
+					MessageBox.Show("エラー：　" + ex.Message);
+				} else {
+					MessageBox.Show("Error: " + ex.Message);
+				}
+			}
+		}
+
+		private void EnDecrypt(byte[] save) {
+			uint seed = BitConverter.ToUInt32(save, 0x1554);
+			for (int i = 0; i < 0x73D2; i++) {
+				save[i] ^= (byte)seed;
+			}
+			Array.Copy(BitConverter.GetBytes(seed), 0, save, 0x1554, sizeof(uint));
+		}
+
+		private int GetShuffledAddr(byte[] save, int addr) {
+			int offset = BitConverter.ToInt32(save, 0x1550) & 0x1FC;
+			return addr + offset;
+		}
+
+		private void SetFlag(byte[] save, int shuffle, int flag, bool value) {
+			int addr = 0x2248 + shuffle + (flag >> 3);
+			int mask = 0x80 >> (flag & 0x7);
+			if (value) {
+				save[addr] |= (byte)mask;
+			} else {
+				save[addr] ^= (byte)~mask;
+			}
+		}
+
+		private uint CalcChecksum(byte[] save, int shuffle, bool eng, bool bm) {
+			uint checksum = bm ? (uint)0x22 : (uint)0x16;
+			for (int i = eng ? 0 : 1; i < 0x73D2; i++) {
+				if (i == 0x21E8 + shuffle) {
+					i += 3;
+					continue;
+				}
+				checksum += save[i];
+			}
+			return checksum;
+		}
+
+		private bool CheckSaveString(byte[] save, int addr, string str) {
+			byte[] bytes = new ASCIIEncoding().GetBytes(str);
+			for (int i = 0; i < bytes.Length; i++) {
+				if (save[addr + i] != bytes[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private void WriteMsgData(byte[] save, GameVersion ver) {
+			for (int i = 0; i < 6; i++) {
+				for (int j = 0; j < 0x5C; j += 2) {
+					ushort s = BitConverter.ToUInt16(MSG_DATA, j);
+					if (j == 0x26 || j == 0x2E || j == 0x3E) {
+						s += (ushort)(i << 6);
+					}
+					if (j == 0x58) {
+						switch (ver) {
+						case GameVersion.EXE4v10:
+							s = 0x64EF;
+							break;
+						case GameVersion.EXE4v11:
+							s = 0x64F3;
+							break;
+						case GameVersion.MMBN4:
+							s = 0x6513;
+							break;
+						}
+					}
+					save[0x1EA0 + i * 0x5C + j] = (byte)s;
+					save[0x1EA0 + i * 0x5C + j + 1] = (byte)(s >> 8);
+				}
+			}
+		}
+	}
+}
